b81d40546240020257bf52397e5955113477659a
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-05-11 01:50:02 +0530

    Initial phase where we convert a youtube video to a .wav audio for transcription using link by Yt_DLP

---
+++ utils/audio_processor.py
@@ -0,0 +1,29 @@
+import os
+
+import yt_dlp
+from pydub import AudioSegment
+
+DOWNLOAD_DIR = 'downloads'
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+def download_youtube_audio(url: str) ->str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": output_path,
+        "ffmpeg_location": r"C:\ffmpeg\bin",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+                "preferredquality": "192",
+            }
+        ],
+        "quiet": True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
+    return filename
+
+print(download_youtube_audio("https://youtu.be/7HSSR1n8dgc?si=iLO0cQmRV_znBeen"))
