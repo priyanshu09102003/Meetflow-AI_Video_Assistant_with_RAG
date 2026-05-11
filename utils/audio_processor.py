6ca35585a250707fadde91e4ad3fb60cda75d673
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-05-11 15:59:52 +0530

    Functions to convert the youtube links to .wav, improving quality, converting local files to .wav and audio chunking to send to whisper.ai, Trigger function  to trigger the endpoints

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -6,6 +6,8 @@ from pydub import AudioSegment
 DOWNLOAD_DIR = 'downloads'
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
+#Converts youtube video links to a wav format for transcription
+
 def download_youtube_audio(url: str) ->str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
     ydl_opts = {
@@ -26,4 +28,49 @@ def download_youtube_audio(url: str) ->str:
         filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
     return filename
 
-print(download_youtube_audio("https://youtu.be/7HSSR1n8dgc?si=iLO0cQmRV_znBeen"))
+
+#Converts files(mp4, mp3, etc) into .wav format
+
+def convert_to_wav(input_path: str) -> str:
+    """Convert any audio/video file to WAV format using pydub."""
+    output_path = os.path.splitext(input_path)[0] + "_converted.wav"
+    audio = AudioSegment.from_file(input_path)
+    audio = audio.set_channels(1).set_frame_rate(16000) #Sets it to mono-audio #16khz
+    audio.export(output_path, format="wav")
+    return output_path
+
+
+#Converts audio to chunks (chunking - 10mins each chnunk)
+
+def chunk_audio(wav_path: str, chunk_minutes: int = 10) -> list:
+    audio = AudioSegment.from_wav(wav_path)
+    chunk_ms = chunk_minutes * 60 * 1000 #Converting to m milliseconds
+
+    chunks = []
+
+    for i,start in enumerate(range(0, len(audio), chunk_ms)):
+        chunk = audio[start : start + chunk_ms]
+        chunk_path = f"{wav_path}_chunk_{i}.wav"
+        chunk.export(chunk_path, format = "wav")
+
+        chunks.append(chunk_path)
+
+    return chunks
+
+
+#Trigger function to trigger every conversion function in the application
+def process_input(source : str) -> list:
+    if source.startswith("http://") or source.startswith("https://"):
+        print("Detected YouTube URL. Downloading audio...")
+        wav_path = download_youtube_audio(source)
+    else:
+        print("Local file detected. Converting to .wav ...")
+        wav_path = convert_to_wav(source)
+    
+    print("Chunking audio...")
+    chunks = chunk_audio(wav_path)
+    print(f"Audio ready - {len(chunks)} chunk(s) created.")
+    return chunks
+
+
+
